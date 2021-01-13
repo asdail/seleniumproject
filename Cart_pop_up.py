da2cd830ad66a7f6cbfc5767ea0e8400139a8c80
yoavl <yoavlv122gmail.com>
2021-01-13 15:49:30 +0200

    new

--- Cart_pop_up.py
+++ Cart_pop_up.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class cart_pop_up:
     def __init__(self,driver):
         self.driver = driver
