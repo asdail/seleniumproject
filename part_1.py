6f5dc4728244cbad841496746ee7d8745fa6f088
yoavl <yoavlv122gmail.com>
2021-01-14 10:49:40 +0200

    new

--- part_1.py
+++ part_1.py
@@ -14,8 +14,8 @@ class AOSProject(unittest.TestCase):
         self.driver.get("https://www.advantageonlineshopping.com/#/")
         self.driver.maximize_window()
 
-    def add_two_products(self):
-        main_page.tablets()
+    def exercise_1(self):
+        self.driver = main_page.tablets()
         Tablets.hp_eletepad()
         hp_ElitePad.color_black()
         hp_ElitePad.quantity("2")
@@ -31,6 +31,29 @@ class AOSProject(unittest.TestCase):
         main_page.cart()
         self.count = cart_pop_up.total_items_in_cart()
         self.assertTrue(self.count,"7 Item" )
+
+
+    def exercise_2(self):
+        main_page.tablets()
+        Tablets.hp_eletepad()
+        hp_ElitePad.color_black()
+        hp_ElitePad.quantity("2")
+        hp_ElitePad.add_to_cart()
+        main_page.back_to_main()
+
+        main_page.tablets()
+        Tablets.hp_pro()
+        hp_pro.color_gray()
+        hp_pro.quantity("3")
+        hp_pro.add_to_cart()
+        main_page.back_to_main()
+        main_page.cart()
+        main_page.back_to_main()
+
+
+        main_page.tablets()
+        Tablets.hp_elite()
+        hp
 
 
 
