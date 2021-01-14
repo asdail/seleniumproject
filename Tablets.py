33b9b4cde2c6261ce252f5119b07a2ff04e5fe12
Amit Jonas <amitjonas1@gmail.com>
2021-01-14 09:58:40 +0200

    'Tablets' classs edited

--- Tablets.py
+++ Tablets.py
@@ -5,6 +5,9 @@ class Tablets:
     def __init__(self, driver):
         self.driver = driver
 
+    def home(self):
+            return self.driver.find_element_by_link_text('HOME')
+
     def hp_eletepad(self):
         return self.driver.find_element_by_id('16').click()
 
@@ -13,6 +16,3 @@ class Tablets:
 
     def hp_pro(self):
         return self.driver.find_element_by_id('18').click()
-
-    def home(self):
-        return self.driver.find_element_by_link_text('HOME')
