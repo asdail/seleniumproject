18d96dc3dc21099b553718416fb7e063f2be47b4
yoavl <yoavlv122gmail.com>
2021-01-20 11:24:34 +0200

    new

--- Tablets.py
+++ Tablets.py
@@ -3,20 +3,17 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+# Class for tablets page
 class Tablets:
 
     def __init__(self, driver):
         self.driver = driver
-
-
 
     def home(self):
         return self.driver.find_element_by_link_text('HOME')
 
     def wait_tablets(self):
         return WebDriverWait(self.driver,20).until(EC.visibility_of_element_located((By.CSS_SELECTOR,"img[id='16']")))
-
 
     def hp_elitepad(self):
         return self.driver.find_element_by_id("16").click()
